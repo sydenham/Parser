de9c56d9da9a80bbfc8aa7f3ec1a04450b3d0667
sydenham <mszarek@interia.pl>
2018-11-24 15:23:04 +0100

    db_connected_and_data_sent

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import java.net.UnknownHostException;
-import java.util.Date;
 
 import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.json.*;
-import com.mongodb.util.JSON;
+
 
 public class Main {
     public static void main(String... args){
@@ -14,26 +11,16 @@ public class Main {
         reader.readFile(args[0]);
 for (String [] line : reader.list){
 
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        MongoDatabase database = mongoClient.getDatabase("logDB");
+       MongoCollection<Document> collection = database.getCollection("logs");
+        Document log = new Document();
+        log.append("Nazwa pola ", line[0]);
+        log.append("Wartosc ", line[1].replace("\"", ""));
+        log.append("Data ", DateFormatter.stringToDate(line[2]));
+        collection.insertOne(log);
 
-    JSONObject obj = new JSONObject();
-    try {
-        obj.put("Nazwa pola ", line[0])
-                .put("Wartosc ", line[1].replace("\"", ""))
-                .put("Data ", DateFormatter.stringToDate(line[2]));
-
-    MongoClient mongoClient = new MongoClient("localhost", 27017);
-    MongoDatabase database = mongoClient.getDatabase("logDB");
-    MongoCollection collection = database.getCollection("logs");
-    Document log = new Document();
-    log.put("Nazwa pola ", line[0]);
-    log.put("Wartosc ", line[1].replace("\"", ""));
-    log.put("Data ", DateFormatter.stringToDate(line[2]));
-    collection.insertOne(log);
-
-    } catch (JSONException e) {
-        e.printStackTrace();
-    }
-System.out.println(obj);
+        mongoClient.close();
 
 }
 
